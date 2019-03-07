e6255b145df496fa192e7d8092d0435347f108a8
Jason <jasonmoramora@outlook.com>
2019-03-07 18:17:43 -0500

    tercer commit

--- routes/articles.js
+++ routes/articles.js
@@ -9,26 +9,32 @@ const url = 'mongodb://localhost:27017';
 // Database Name
 const dbName = 'almacen';
 
+// Use connect method to connect to the Server
+MongoClient.connect(url, (err, client)=> {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");      
+    client.close();
+});
+
 /*methods*/
 router.get('/articles', (req, res)=>{
     MongoClient.connect(url, (err, client)=> {
         assert.equal(null, err);
-        const db = client.db(dbName);   
+        let db = client.db(dbName);   
         findDocuments(db, (records)=> {
             res.send(records);
             client.close();
         });    
     });    
 });
+
 router.post('/articles', (req, res)=>{
-    let objArticle = req.body;
-    MongoClient.connect(url, (err, client)=> {
+    let myJSON = req.body;
+    MongoClient.connect(url,{ useNewUrlParser: true }, (err, client)=> {
         assert.equal(null, err);
         const db = client.db(dbName);   
-        db.collection('articulos').insertOne(objArticle, (err, r)=>{
-            assert.equal(null, err);
-            assert.equal(1, r.insertedCount);
-            res.send('Datos ingresados correctamente');
+        insertDocuments(db, myJSON, (r)=>{
+            res.send('Datos registrados');
             client.close();
         });
     });    
@@ -37,13 +43,19 @@ router.post('/articles', (req, res)=>{
 //request db
 const findDocuments = (db, callback)=> {
     let collection = db.collection('articulos');    
-    collection
-    .find({})
-    .project({ '_id': 0 })
-    .toArray((err, docs)=> {
+    collection.find({}).project({ '_id': 0 }).toArray((err, docs)=> {
         assert.equal(err, null);
         callback(docs);
     });
 };
+const insertDocuments = (db, data, callback)=>{
+    let collection = db.collection('articulos');
+    collection.insertOne(data, (err, r)=>{
+        assert.equal(err, null);
+        callback(r);
+    });
+};
+
+
 
 module.exports = router;
